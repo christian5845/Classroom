552a949da2a6e62227232eb14ce2dae2d2b61413
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2026-08-31 11:28:33 +0200

    task 2

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ClassRoom
+{
+    public string CLassName { get; }
+    public List<Student> StudentList { get; set; }
+    public DateTime SemesterStart { get; }
+
+    public ClassRoom(string className, List<Student> studentList, DateTime semesterStart)
+    {
+        className = className;
+        studentList = new List<Student>();
+        semesterStart = SemesterStart;
+    }
+
+
+}
+    
+
--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 public class Student
 {
-    public string Name { get; }
-    public int BirthMonth { get; }
-    public int BirthYear { get; }
+    public string Name { get; private set; }
+    public int BirthMonth { get; private set; }
+    public int BirthYear { get; private set; }
 
 
     public Student (string name, int birthMonth, int birthYear)
