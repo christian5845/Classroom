d3932e39517b15ee1dec981c7189ae5fe9bdf49c
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2026-08-31 11:19:37 +0200

    Task 1 færdig

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Student
+{
+    public string Name { get; }
+    public int BirthMonth { get; }
+    public int BirthYear { get; }
+
+
+    public Student (string name, int birthMonth, int birthYear)
+    {
+        name = Name;
+        birthMonth = BirthMonth;
+        birthYear = BirthYear;
+
+    }
+}
+
