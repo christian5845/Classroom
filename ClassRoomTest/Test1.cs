f67d6e303e415bac8f8e23de8bc4a44dace7468a
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2026-09-07 11:06:06 +0200

    added unittest

---
+++ ClassRoomTest/Test1.cs
@@ -0,0 +1,26 @@
+﻿namespace ClassRoomTest
+{
+    [TestClass]
+    public sealed class ClassRoomTestSeason
+    {
+        [TestMethod]
+        [DataRow("G", 2, 3999, "Winter")]
+        [DataRow("Rasmus", 4, 2004, "Spring")]
+
+        
+        public void TestStuderende_Årstid_Vinter(string name, int month, int year, string expectedSeason)
+        {
+            //arange
+            Student student = new Student(name, month, year);
+
+            //Act
+            string årstid = student.Season();
+
+            //Assert
+            Assert.AreEqual(expectedSeason, årstid);
+        }
+
+
+
+    }
+}
